1775e418905caca33ce52f929679562de2b7f616
cuatrotales <jfquinterogu14@gmail.com>
2022-10-22 13:01:45 -0500

    Sistema de notas completo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer class="container fixed-bottom bg-white">
+      <p class="float-end">
+        <a href="#title">Volver arriba</a>
+      </p>
+      <p>
+        &copy; 2022 Cuatrotales, Dev. &middot; <a href="https://github.com/cuatrotales" target="_blank">Github</a> &middot;
+      </p>
+    </footer>
+  );
+}
